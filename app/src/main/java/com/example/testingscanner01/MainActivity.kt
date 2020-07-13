b221fb192d5f744acaada19ddbf7416d2a48bfe1
vickyyy19 <56965773+vickyyy19@users.noreply.github.com>
2020-07-13 20:50:57 +0800

    Initial commit

---
+++ app/src/main/java/com/example/testingscanner01/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.testingscanner01
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.zxing.integration.android.IntentIntegrator
+import com.google.zxing.integration.android.IntentResult
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnScan.setOnClickListener {
+            val scanner = IntentIntegrator(this)
+         // scanner.setBeepEnabled(false)
+            scanner.initiateScan()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if(resultCode == Activity.RESULT_OK){
+            val result:IntentResult = IntentIntegrator.parseActivityResult(requestCode,resultCode,data)
+            if(result != null) {
+                if(result.contents == null) {
+                    Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(this, "Scanned: " + result.contents, Toast.LENGTH_LONG).show();
+                }
+            } else {
+                super.onActivityResult(requestCode, resultCode, data);
+            }
+        }
+    }
+}
